dfd2c51ea029739e2962762b287bede55bd31c19
LedAndris <ledererandras2004@gmail.com>
2021-07-20 02:12:44 +0200

    add dark reader check

--- ifuseethisthenuraskid.js
+++ ifuseethisthenuraskid.js
@@ -10,6 +10,10 @@ ready(() => {
     const shouldDestroy = urlSearchParams.get("destroy") == "1"
     const destroy = document.getElementById("dumbfuck")
     const google = document.getElementById("usegoogle")
+    const usingDarkReader = document.getElementsByClassName("data-darkreader-mode")[0]
+    if (usingDarkReader) {
+        alert("Hey turn off your dark reader!")
+    }
 
     destroy.innerHTML = shouldDestroy ? "u dumb fuck" : ""
     google.innerHTML = shouldGoogle ? "&#10024; GETTING GOOD &#10024; and using fucking <a href=\"https://google.com\">google</a> to answer your miserable shit question" : "&#10024; GETTING GOOD &#10024;"
